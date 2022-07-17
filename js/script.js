57124e518c30ee1807417598fdb349bb90f33e0d
FlabyKare <beslansoblirov074@gmail.com>
2022-07-17 18:12:47 +0300

    Demo_for_mobile

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
-
-
-
-
-
-const accordion_header = document.getElementById('header');
+$(document).ready(function(){
+    let burger = $('.burger')
+    let menu_wrapper = $('.menu_wrapper')
+    let body = $('body')
+    let body_cont = $('.body_container')
+    
+    burger.click(function(){
+        menu_wrapper.toggleClass('menu_open')
+        burger.toggleClass('transform_rotate')
+        body.toggleClass('overflow_hidden')
+        body_cont.toggleClass('d-none')
+    
+    })
+    const accordion_header = document.getElementById('header');
 
 accordion_header.addEventListener("click", () => {
 	$("#content").toggleClass('hide');
@@ -32,6 +40,12 @@ accordion_header2.addEventListener("click", () => {
 	$("#content2").toggleClass('hide2');
 	$("#header2").toggleClass('active2');
 })
+
+    });
+
+
+
+
 
 
 
