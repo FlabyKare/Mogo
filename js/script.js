da0778664540f888eae72d6bfb40e8b205e667ce
FlabyKare <beslansoblirov074@gmail.com>
2022-07-18 12:48:56 +0300

    added_transition_for_sections

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     let menu_wrapper = $('.menu_wrapper')
     let body = $('body')
     let body_cont = $('.body_container')
-    
+    let menu_link = $('.menu_link')
     burger.click(function(){
         menu_wrapper.toggleClass('menu_open')
         burger.toggleClass('transform_rotate')
@@ -11,6 +11,39 @@ $(document).ready(function(){
         body_cont.toggleClass('d-none')
     
     })
+    menu_link.click(function(){
+        menu_wrapper.toggleClass('menu_open')
+        burger.toggleClass('transform_rotate')
+        body.toggleClass('overflow_hidden')
+        body_cont.toggleClass('d-none')
+    
+    })
+    
+
+
+
+
+    function onEntry(entry) {
+        entry.forEach(change => {
+          if (change.isIntersecting) {
+           change.target.classList.add('element-show');
+          }
+        });
+      }
+      
+      let options = {
+        threshold: [0.5] };
+      let observer = new IntersectionObserver(onEntry, options);
+      let elements = document.querySelectorAll('.element-animation');
+      
+      for (let elm of elements) {
+        observer.observe(elm);
+      }
+
+
+
+
+
     const accordion_header = document.getElementById('header');
 
 accordion_header.addEventListener("click", () => {
